1675d276078b9d4df57052f9014959a9fb6bd9b8
Camilla Gomes <camilla.gomeseugenio2005@gmail.com>
2024-11-27 19:40:00 -0300

    Criando projeto

---
+++ src/main/java/com/trabalho/topicosjava/TopicosjavaApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.topicosjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicosjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicosjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabalho/topicosjava/TopicosjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.topicosjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicosjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
